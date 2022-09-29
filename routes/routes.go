df33b8f886ef1824bb0290e9b136572d4877cce8
Carlos Eduardo Guimarães <eduardocarlos456@gmail.com>
2022-09-29 20:58:28 -0300

    Adicionado o CORS na aplicação; Adicionado um front end para visualizar as personalidades cadastradas;

--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,17 @@ package routes
 
 import (
 	"api-rest-go/controllers"
+	"api-rest-go/middleware"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
@@ -17,5 +20,5 @@ func HandleRequest() {
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
